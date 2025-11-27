e3c30312fe281501089576afae00628e77d0831e
Miguel <miguelcaslp@gmail.com>
2025-11-27 01:29:58 +0100

    test vercel 13

--- api/index.js
+++ api/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { createClient } = require("@supabase/supabase-js");
+const cors = require('cors');
+require("dotenv").config();
+
 const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:4200",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 
 app.get("/test", (req, res) => {
-  res.json({ ok: test });
+  res.json({ ok: "test" });
 });
 
 
 app.get("/", (req, res) => {
-  res.json({ ok: Incio });
+  res.json({ ok: "Inicio" });
 });
 
 app.listen(3000, ()=>{
